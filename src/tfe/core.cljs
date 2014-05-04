407ed9bd11b3814c3ee00ccba0db462eaded069c
Andrew Kiellor <akiellor@gmail.com>
2014-05-04 00:22:33 -0700

    Added hooks for manipulating game state.

--- src/tfe/core.cljs
+++ src/tfe/core.cljs
@@ -7,10 +7,39 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:board [[nil nil nil nil]
+(def app-state (atom {:state :playing
+                      :board [[nil nil nil nil]
                               [nil nil nil   2]
                               [nil nil nil   2]
                               [nil nil nil nil]]}))
+
+(defn next-board [direction board]
+  (vec (apply map vector board)))
+
+(defn next-state [board]
+  :playing)
+
+(defn next-game [direction game]
+  (let [board (:board game)
+        state (:state game)]
+    (assoc game
+           :board (next-board direction board)
+           :state (next-state (next-board direction board)))))
+
+(defn code->direction [key-code]
+  (case key-code
+    37 :left
+    38 :up
+    39 :right
+    40 :down
+    nil))
+
+(defn handle-keybindings [event]
+  (let [keyCode (.-keyCode event)
+        direction (code->direction keyCode)]
+    (swap! app-state #(next-game direction %))))
+
+(events/listen js/document "keyup" (comp println handle-keybindings))
 
 (defn cell [value]
   (dom/span #js {:className "cell"}  value))
