450c5827181c47e3bee25475be99bb2d0bf92725
Andrew Kiellor <akiellor@gmail.com>
2014-05-04 22:18:55 -0700

    Basic board movement.

--- src/tfe/core.cljs
+++ src/tfe/core.cljs
@@ -2,12 +2,44 @@
 
 (def app-state (atom {:state :playing
                       :board [[nil nil nil nil]
-                              [nil nil nil   2]
+                              [2 nil nil   2]
                               [nil nil nil   2]
                               [nil nil nil nil]]}))
 
-(defn next-board [direction board]
-  (vec (apply map vector board)))
+(defn strip [row]
+  (filter identity row))
+
+(defn pad [row]
+  (take 4 (concat row (repeat nil))))
+
+(defn collapse [row]
+  row)
+
+(defn pack [board]
+  (map #(-> % strip collapse pad) board))
+
+(defn rotate [board]
+  (apply map vector board))
+
+(defn flip [board]
+  (map (comp vec reverse) board))
+
+(defmulti next-board (fn [d b] d))
+
+(defmethod next-board :left [_ board]
+  (pack board))
+
+(defmethod next-board :right [_ board]
+  (-> board flip pack flip))
+
+(defmethod next-board :up [_ board]
+  (-> board rotate pack rotate))
+
+(defmethod next-board :down [_ board]
+  (-> board rotate flip pack flip rotate))
+
+(defmethod next-board nil [_ board]
+  board)
 
 (defn next-state [board]
   :playing)
