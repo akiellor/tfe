8ebb531de814c36f6c9cf8c34215f3b7a4cd192c
Andrew Kiellor <akiellor@gmail.com>
2014-05-14 22:04:46 -0700

    Upgraded to java 1.7

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :java-target "1.7"
   :dependencies [[http-kit "2.1.18"]
                  [ring/ring-core "1.2.2"]
                  [org.clojure/clojure "1.5.1"]
